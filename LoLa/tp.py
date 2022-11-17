30039fc8d611864254f81c50784b7e4ec445ece2
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2022-11-17 22:20:41 +0100

    Create tp.py

---
+++ LoLa/tp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+Utility functions used for Logic & Language course at Utrecht University
+contact: Lasha.Abzianidze@uu.nl
+"""
+
+
+def tableau_prove(conclusion: str, premises: List[str] = [], verbose: bool = False) -> bool:
+    """ 
+    Given a conclusion and a list of premises, builds a tableau and
+    detects whether the premises entail the conclusion.
+    Returns a boolean value and optionally prints the tableau structure
+    """
+    str2exp = nltk.sem.Expression.fromstring
+    c = str2exp(conclusion)
+    ps = [ str2exp(p) for p in premises ] 
+    return nltk.TableauProver().prove(c, ps, verbose=verbose)

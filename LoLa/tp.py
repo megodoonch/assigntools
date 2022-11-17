ef42ef00ab02b45f4fe8bb2b51679abd497bc551
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2022-11-18 00:42:27 +0100

    Update tp.py

--- LoLa/tp.py
+++ LoLa/tp.py
@@ -6,9 +6,10 @@ Utility functions used for Logic & Language course at Utrecht University
 contact: Lasha.Abzianidze@uu.nl
 """
 
-from typing import List
+from typing import List, Tuple, Dict
 import nltk
 
+#########################################################
 def tableau_prove(conclusion: str, premises: List[str] = [], verbose: bool = False) -> bool:
     """ 
     Given a conclusion and a list of premises, builds a tableau and
@@ -19,3 +20,24 @@ def tableau_prove(conclusion: str, premises: List[str] = [], verbose: bool = Fal
     c = str2exp(conclusion)
     ps = [ str2exp(p) for p in premises ] 
     return nltk.TableauProver().prove(c, ps, verbose=verbose)
+
+#########################################################
+def make_vars_consistent(prop_maps: List[Tuple[str, Dict[str,str]]], 
+                         prop_letter: str = 'Q') \
+    -> Tuple[List[str], Dict[str,str]]:
+    """
+    Takes a list of pairs of a propositional formuals and the mapping from
+    propositional letters to the natural language sentences.
+    It renames all propositional letters in the formulas and makes sure that 
+    the mapping from letter to sentences is one to one.
+    """
+    all_sents = [ v for (_, m) in prop_maps for k, v in m.items() ]
+    sent2index = { s: i for i, s in enumerate(sorted(all_sents), start=1) }
+    mapping, props = dict(), []
+    for prop, m in prop_maps:
+        for pi, sent in m.items():
+            qi = f"{prop_letter}{sent2index[sent]}"
+            prop = re.sub(fr'\b{pi}\b', qi, prop)
+            props.append(prop)
+            mapping[qi] = sent
+    return props, mapping    

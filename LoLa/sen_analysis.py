5fb632d8ece843ebff407ba60ec1dea193af5edf
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2022-12-10 13:13:59 +0100

    Update sen_analysis.py

--- LoLa/sen_analysis.py
+++ LoLa/sen_analysis.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
+import spacy
+
+tokenized2Doc(raw, tokens, spacy_pipeline):
+    """
+    Takes raw text and its tokenized version and returns spaCy's Doc object 
+    """
+    # TODO: initialize spaces arg too, now it defults to the list of True
+    return Doc(spacy_pipeline.vocab, words=tokens)

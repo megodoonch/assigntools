eecd5a8c83b2219e10ef21f85d64523fb983e6f0
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2022-12-10 16:24:34 +0100

    Update sen_analysis.py

--- LoLa/sen_analysis.py
+++ LoLa/sen_analysis.py
@@ -2,6 +2,8 @@
 # -*- coding: utf8 -*-
 
 import spacy
+import itertools
+from tqdm import tqdm
 
 def tokenized2Doc(raw, tokens, spacy_pipeline):
     """
@@ -9,3 +11,21 @@ def tokenized2Doc(raw, tokens, spacy_pipeline):
     """
     # TODO: initialize spaces arg too, now it defults to the list of True
     return spacy.tokens.Doc(spacy_pipeline.vocab, words=tokens)
+
+
+def spacy_sen_context(spacy_nlp, sen_context_dict, disable_components=[], n=0):
+    """
+    Takes spacy_nlp pipeline and processes sentences in sen_context_dict
+    where the latter is {sen->context}, context carrying additional info about sentences.
+    n - does cut off
+    disable_components - a list of spacy pipeline components that will be disabled during processing\
+    Returns sen->anno dict where anno has an additional key 'spacy' with value of spacy Doc
+    """
+    if not n: n = len(sen_context_dict)
+    doc_sen_anno = [ (tokenized2Doc(s, a['tok'], spacy_nlp), (s, a)) for s, a in itertools.islice(sen_context_dict.items(), n) ]
+    sen_anno = dict() # a dictionary with sentence keys and anno gets additional key for 'spacy' that has spacy Doc as value
+    with spacy_nlp.select_pipes(disable=disable_components):
+        for doc, (sen, anno) in tqdm(spacy_nlp.pipe(doc_sen_anno, as_tuples=True)):
+            sen_anno[sen] = anno
+            sen_anno[sen]['spacy'] = doc
+    return sen_anno
